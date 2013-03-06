5ed422ae81b2385bc4bcc359e7d8497be895ba69
Johan Martinsson <jmartinsson.ext@orange.com>
2013-03-06 23:03:55 +0100

    ajout cas de reutilisation de builder

--- src/builder/simple/Test.java
+++ src/builder/simple/Test.java
@@ -6,7 +6,9 @@ import static org.junit.Assert.assertEquals;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.junit.Assert;
+import org.junit.Ignore;
+
+import builder.simple.Menu.Builder;
 
 
 public class Test {
@@ -25,15 +27,16 @@ public class Test {
 	@org.junit.Test
 	public void petit_repas_gourmand() throws Exception {
 		Menu menu = unMenuAvec()
-				.entree("salade")
-				.plat("Filet rossini")
-				.dessert("crème brûlée")
-				.vin()
-				.fromage()
-				.commander();
+					.entree("salade")
+					.plat("Filet rossini")
+					.dessert("crème brûlée")
+					.vin()
+					.fromage()
+					.commander();
 		
 		assertEquals("Menu [entree=salade, plat=Filet rossini, dessert=crème brûlée, fromage=true, cafe=false, vin=true]", menu.toString());
 	}
+
 
 	@org.junit.Test
 	public void mariage_complet() throws Exception {
@@ -63,5 +66,18 @@ public class Test {
 		
 	}
 
+	@org.junit.Test @Ignore //TODO faire passer le test
+	public void reutiliser_un_builder() throws Exception {
+		Builder menuDeBase = unMenuAvec()
+							 .entree("salade")
+							 .plat("Filet rossini");
+		
+		Menu menuAvecDessert = menuDeBase.dessert("mousse au chocolat").commander();
+		Menu menuAvecCafe    = menuDeBase.cafe().commander();
+		
+		assertEquals("Menu [entree=salade, plat=Filet rossini, dessert=mousse au chocolat, fromage=false, cafe=false, vin=false]", menuAvecDessert.toString());
+		assertEquals("Menu [entree=salade, plat=Filet rossini, dessert=null, fromage=false, cafe=true, vin=false]", menuAvecCafe.toString());
+	}
+	
 
 }
